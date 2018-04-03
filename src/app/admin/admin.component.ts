1ec7c11843a3b9397054183a4e18cae860763eb5
Masen Matthews <masenmatthews@gmail.com>
2018-04-02 21:34:54 -0700

    finish adding Tuesday homework - issue in app.service.ts folder

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -16,8 +16,8 @@ export class AdminComponent {
   ngOnInit() {
   }
 
-  submitForm(title: string, artist: string, description: string) {
-    var newAlbum: Album = new Album(title, artist, description);
+  submitForm(title: string, artist: string, description: string, id: number) {
+    var newAlbum: Album = new Album(title, artist, description, id);
     this.albumService.addAlbum(newAlbum);
   }
 
---
+++ src/app/edit-album/edit-album.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AlbumService } from '../album.service';
+
+@Component({
+  selector: 'app-edit-album',
+  templateUrl: './edit-album.component.html',
+  styleUrls: ['./edit-album.component.css'],
+  providers: [AlbumService]
+})
+export class EditAlbumComponent implements OnInit {
+  @Input() selectedAlbum;
+
+  constructor(private albumService: AlbumService) { }
+
+  ngOnInit() {
+  }
+
+  beginDeletingAlbum(albumToDelete){
+    if(confirm("Are you sure you want to delete this item from the inventory?")){
+      this.albumService.deleteAlbum(albumToDelete);
+    }
+  }
+
+}
